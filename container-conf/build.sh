cae09a3950b25bb167d51c9a67974d3d8b00da12
robnagler <github@q33.us>
2019-11-17 21:32:39 +0000

    cuda-drivers

---
+++ container-conf/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+build_image_base=radiasoft/beamsim-jupyter
+build_is_public=1
+
+build_as_root() {
+    rpm -i https://developer.download.nvidia.com/compute/cuda/repos/fedora29/x86_64/cuda-repo-fedora29-10.1.243-1.x86_64.rpm
+    dnf install -y kmodtool kernel-devel cuda-drivers
+}
