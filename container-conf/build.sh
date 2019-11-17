c070093904580cee9221171c95cc0899b7cd30d9
robnagler <github@q33.us>
2019-11-17 22:12:29 +0000

    builds happen as root, but need to set build_run_user

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 build_image_base=radiasoft/beamsim-jupyter
 build_is_public=1
+build_dockerfile_aux="USER $build_run_user"
 
 build_as_root() {
     rpm -i https://developer.download.nvidia.com/compute/cuda/repos/fedora29/x86_64/cuda-repo-fedora29-10.1.243-1.x86_64.rpm
